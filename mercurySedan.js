b1d035c51669d6643ad3e5afabe01d04fb3d6a5f
keylat160 <keylat160@gmail.com>
2023-12-11 18:35:31 -0800

    first commit

--- mercurySedan.js
+++ mercurySedan.js
@@ -7,12 +7,42 @@ console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
-
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+class Sedan {
+    constructor(name, maximumPassenger, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService){
+      this.name = initialValue;
+      this.maximumPassenger = 5;
+      this.passenger = 0;
+      this.numberOfWheels = 4;
+      this.maximumSpeed = 160;
+      this.fuel = 10;
+      this.scheduleService = false;
+}
+loadPassenger() {
 
+}
+
+start() {
+    if (this.parameter > 0) {
+        return this.started = true;
+        console.log("engine started...!!!");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start...");
+    }
+}
+
+scheduleService() {
+   if(this.mileage > 30000) {
+      return this.mileage = true;
+      console.log("Time for maintenance");
+   }  else {
+       return this.mileage = false;
+       console.log("No need maintenance");
+   }
+}
+}
 
 
 
